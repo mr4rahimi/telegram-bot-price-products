2673bd3646630f13addacbeef87c0b4ca1457ef1
mr4rahimi <mr4.rahimi@gmail.com>
2026-02-26 17:24:30 +0330

    add aiogram

--- scripts/test_aiogram_getme.py
+++ scripts/test_aiogram_getme.py
@@ -5,33 +5,22 @@ from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
-from aiogram.utils.backoff import BackoffConfig
 
 from app.config import settings
-from app.bot.dispatcher import setup_dispatcher
 
 
-async def main() -> None:
-    proxy_url = "http://127.0.0.1:10808/" 
+async def main():
     session = AiohttpSession(
-        proxy=proxy_url,
+        proxy="http://127.0.0.1:10808/",
         timeout=ClientTimeout(total=60),
     )
-
     bot = Bot(
         token=settings.bot_token,
         session=session,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
-
-    dp = setup_dispatcher()
-
-    await dp.start_polling(
-        bot,
-        polling_timeout=60,
-        request_timeout=60,
-        backoff_config=BackoffConfig(min_delay=1.0, max_delay=20.0, factor=1.5, jitter=0.1),
-    )
+    me = await bot.get_me()
+    print(me)
 
 
 if __name__ == "__main__":
